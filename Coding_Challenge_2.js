ab0b8e5b2df93080e7c34d8bb54b7cf90334c889
LuisVieira0701 <luisvieira@usf.edu>
2024-09-08 10:32:15 -0400

    Task 2 Add output functionality to display bill, tip, total

--- Coding_Challenge_2.js
+++ Coding_Challenge_2.js
@@ -1,3 +1,5 @@
 // Initial Logic for Calculating a Tip Using Ternary Operations
-let Bill = 275
-let tip = Bill >= 50 && Bill <=300 ? (Bill * 0.15) : (Bill * 0.20)
+let Bill = 275;
+let tip = Bill >= 50 && Bill <=300 ? (Bill * 0.15) : (Bill * 0.20);
+
+    console.log(`Thanks for dining at Reeds Restaurant. Your Bill total is $${Bill}, the tip was $${tip}, and your total amount is $${Bill + tip}`);
