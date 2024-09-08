de99910df8a1ed5d002041ad4670b45dbdd1627d
LuisVieira0701 <luisvieira@usf.edu>
2024-09-08 12:23:17 -0400

    Task 3 - Calculate a Function

--- Coding_Challenge_2.js
+++ Coding_Challenge_2.js
@@ -3,3 +3,11 @@ let Bill = 275;
 let tip = Bill >= 50 && Bill <=300 ? (Bill * 0.15) : (Bill * 0.20);
 
     console.log(`Thanks for dining at Reeds Restaurant. Your Bill total is $${Bill}, the tip was $${tip}, and your total amount is $${Bill + tip}`);
+
+//Create a function
+function calculateTip (Bill) {
+    let TipPercent = (Bill >= 50 && Bill <= 300) ? 0.15 : 0.20;
+    return Bill * TipPercent
+}
+
+calculateTip(350)
